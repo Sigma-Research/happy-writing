c502478b6e6fa1f24e60808e5b702630194b6362
sad wang <leown_wang@163.com>
2020-05-06 18:07:24 +0800

    chore: reinit the project structure

--- miniprogram/pages/course/course.js
+++ miniprogram/pages/course/course.js
@@ -1,4 +1,4 @@
-// pages/deployFunctions/deployFunctions.js
+// miniprogram/pages/course/course.js
 Page({
 
   /**
--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -1,4 +1,4 @@
-// pages/chooseLib/chooseLib.js
+// miniprogram/pages/profile/profile.js
 Page({
 
   /**
