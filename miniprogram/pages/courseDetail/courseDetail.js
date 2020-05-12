bb7f1d119cd619c203400c926be54221c4a65522
sad wang <leown_wang@163.com>
2020-05-12 10:59:02 +0800

    styl: some detail

--- miniprogram/pages/courseDetail/courseDetail.js
+++ miniprogram/pages/courseDetail/courseDetail.js
@@ -1,65 +1,30 @@
 // miniprogram/pages/courseDetail/courseDetail.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    disableShowModelState: false,
+    disableShowModelData: null
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   },
@@ -69,12 +34,19 @@ Page({
     })
   },
   learnDisable() {
-    wx.showModal({
-      title: '温馨提示',
-      content: '完成上节课的课程学习才能继续本节课程的学习',
-      confirmText: '知道了',
-      showCancel: false,
-      confirmColor: '#192e4d',
+    this.setData({
+      disableShowModelState: true,
+      disableShowModelData: {
+        type: 'single',
+        title: '温馨提示',
+        content: '完成上一节课程的学习才能继续本节课成的学习',
+        confirmText: '知道了'
+      }
+    })
+  },
+  hiddenDisableShowModel () {
+    this.setData({
+      disableShowModelState: false
     })
   }
 })
