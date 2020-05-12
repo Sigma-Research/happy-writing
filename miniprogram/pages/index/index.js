c7511f6d5ee0b6278ec1178d69299a5679dee627
sad wang <leown_wang@163.com>
2020-05-12 14:20:38 +0800

    refactor: delete the extra cloudFunction in index page

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -10,27 +10,9 @@ Page({
   },
 
   onLoad: function() {
-    if (!wx.cloud) {
-      console.log('没有使用云函数')
-      return
-    }
+
   },
 
-
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-      }
-    })
-  },
   toCoursePoster: () => {
     wx.navigateTo({
       url: '../coursePoster/coursePoster'
