e7127ea9488630f352b8da30ed8907b01cce4d3c
sad wang <leown_wang@163.com>
2020-05-14 14:55:42 +0800

    feat: add the feature demo of video

--- miniprogram/pages/courseLearn/courseLearn.js
+++ miniprogram/pages/courseLearn/courseLearn.js
@@ -1,48 +1,49 @@
 // miniprogram/pages/courseLearn/courseLearn.js
-import createHanziWriterContext from 'hanzi-writer-miniprogram';
+// import createHanziWriterContext from 'hanzi-writer-miniprogram';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    videoFullscreenState: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.writerCtx1 = createHanziWriterContext({
-      id: 'hz-writer1',
-      character: '你',
-      page: this,
-      renderer: 'canvas'
-    });
-    this.writerCtx2 = createHanziWriterContext({
-      id: 'hz-writer2',
-      character: '好',
-      page: this,
-    });
-    this.writerCtx3 = createHanziWriterContext({
-      id: 'hz-writer3',
-      character: '啊',
-      page: this,
-    });
-    this.writerCtx4 = createHanziWriterContext({
-      id: 'hz-writer4',
-      character: '啊',
-      page: this,
-    });
-    this.writerCtx5 = createHanziWriterContext({
-      id: 'hz-writer5',
-      character: '啊',
-      page: this,
-    });
-    this.writerCtx6 = createHanziWriterContext({
-      id: 'hz-writer6',
-      character: '啊',
-      page: this,
-    });
+    // this.writerCtx1 = createHanziWriterContext({
+    //   id: 'hz-writer1',
+    //   character: '你',
+    //   page: this,
+    //   renderer: 'canvas'
+    // });
+    // this.writerCtx2 = createHanziWriterContext({
+    //   id: 'hz-writer2',
+    //   character: '好',
+    //   page: this,
+    // });
+    // this.writerCtx3 = createHanziWriterContext({
+    //   id: 'hz-writer3',
+    //   character: '啊',
+    //   page: this,
+    // });
+    // this.writerCtx4 = createHanziWriterContext({
+    //   id: 'hz-writer4',
+    //   character: '啊',
+    //   page: this,
+    // });
+    // this.writerCtx5 = createHanziWriterContext({
+    //   id: 'hz-writer5',
+    //   character: '啊',
+    //   page: this,
+    // });
+    // this.writerCtx6 = createHanziWriterContext({
+    //   id: 'hz-writer6',
+    //   character: '啊',
+    //   page: this,
+    // });
 
 
 
@@ -61,7 +62,7 @@ Page({
    */
   onShow: function () {
     // You can call any normal HanziWriter method here
-    this.writerCtx1.loopCharacterAnimation();
+    // this.writerCtx1.loopCharacterAnimation();
 
   },
 
@@ -104,5 +105,17 @@ Page({
     wx.navigateTo({
       url: '../homeworkDetail/homeworkDetail'
     })
+  },
+  changeFullscreen: function () {
+    this.setData({
+      videoFullscreenState: !this.data.videoFullscreenState
+    })
+  },
+  createVideo: function () {
+    wx.createVideoContext('testV').play()
+    wx.createVideoContext('testV').requestFullScreen()
+  },
+  videoEnd: function () {
+    wx.createVideoContext('testV').exitFullScreen()
   }
 })
