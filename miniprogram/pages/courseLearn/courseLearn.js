74863c7939256b541e8db00ef1e258b612eae3d0
sad wang <leown_wang@163.com>
2020-05-19 14:49:12 +0800

    refactor: refactor the courseLearn.js

--- miniprogram/pages/courseLearn/courseLearn.js
+++ miniprogram/pages/courseLearn/courseLearn.js
@@ -1,17 +1,9 @@
-// miniprogram/pages/courseLearn/courseLearn.js
+// courseLearn.js
 // import createHanziWriterContext from 'hanzi-writer-miniprogram';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     videoFullscreenState: false
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
     const eventChannel = this.getOpenerEventChannel()
     eventChannel.on('getCourseData', (res) => {
@@ -48,63 +40,24 @@ Page({
     //   character: '啊',
     //   page: this,
     // });
-
-
-
-
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
     // You can call any normal HanziWriter method here
     // this.writerCtx1.loopCharacterAnimation();
-
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
-
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
-
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
-
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
-
   },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
-
   },
-
   toHomeworkDetail: () => {
     wx.navigateTo({
       url: '../homeworkDetail/homeworkDetail'
