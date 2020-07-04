5b4accc404e14c7b6aebdf632e06fb2c432265a7
sad wang <leown_wang@163.com>
2020-07-04 10:00:27 +0800

    style:完成taskList页面样式

--- miniprogram/pages/taskList/taskList.js
+++ miniprogram/pages/taskList/taskList.js
@@ -1,6 +1,22 @@
 Page({
   data: {
-
+    taskList: [
+      {
+        time: '7月6日 星期五',
+        title: '字帖练习任务1',
+        state: '正在进行',
+      },
+      {
+        time: '7月7日 星期六',
+        title: '字帖练习任务2',
+        state: '正在进行',
+      },
+      {
+        time: '7月8日 星期一',
+        title: '字帖练习任务3',
+        state: '正在进行',
+      }
+    ]
   },
   onLoad (options) {
 
