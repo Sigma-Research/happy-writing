bcda2391fab5e030f9ffd2a1e78bca7965ef8ddf
sad wang <leown_wang@163.com>
2020-07-04 16:35:03 +0800

    style: 完成login页面的样式

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -1,6 +1,20 @@
 Page({
   data: {
-
+    schoolList: [
+       '快乐小学',
+       '悲伤小学'
+    ],
+    gradeList: [
+      '六年级',
+      '二年级'
+    ],
+    classList: [
+      '一班',
+      '二班'
+    ],
+    school: null,
+    grade: null,
+    class: null
   },
   onLoad (options) {
 
@@ -25,5 +39,16 @@ Page({
   },
   onShareAppMessage () {
 
+  },
+  changeSchool (e) {
+    this.setData({
+      school: this.data.schoolList[e.detail.value]
+    })
+  },
+  changeGrade (e) {
+    this.setData({
+      grade: this.data.gradeList[e.detail.value[0]],
+      class: this.data.classList[e.detail.value[1]]
+    })
   }
 })
