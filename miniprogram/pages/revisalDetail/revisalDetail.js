fb113f018b7f74de22c2c9ce478ba4fcc0a613f6
sad wang <leown_wang@163.com>
2020-07-06 16:47:46 +0800

    style: 完成作业批改页面样式

---
+++ miniprogram/pages/revisalDetail/revisalDetail.js
@@ -0,0 +1,29 @@
+Page({
+  data: {
+    tab: 'revise'
+  },
+  onLoad (options) {
+
+  },
+  onReady () {
+
+  },
+  onShow () {
+
+  },
+  onHide () {
+
+  },
+  onUnload () {
+
+  },
+  onPullDownRefresh () {
+
+  },
+  onReachBottom () {
+
+  },
+  onShareAppMessage () {
+
+  }
+})
