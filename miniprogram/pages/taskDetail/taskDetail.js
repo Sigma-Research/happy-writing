a1acfa2a7cf4f58ecf31c9ab10aae53db70e9586
sad wang <leown_wang@163.com>
2020-07-06 10:56:44 +0800

    style: 完成 taskDetail 的页面样式

--- miniprogram/pages/taskDetail/taskDetail.js
+++ miniprogram/pages/taskDetail/taskDetail.js
@@ -25,5 +25,30 @@ Page({
   },
   onShareAppMessage () {
 
+  },
+  showTips () {
+    wx.showModal({
+      title: '温馨提示',
+      content: '1. 将写好的字帖平铺在桌子上\n2.拍照时保证手机和字帖平行\n',
+    })
+  },
+  toMytask () {
+    wx.navigateTo({
+      url: '../myTask/myTask'
+    })
+  },
+  uploadTask () {
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success (res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        wx.navigateTo({
+          url: '../myTask/myTask'
+        })
+      }
+    })
   }
 })
